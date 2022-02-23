3d20fb92901d394a2b5990d0dadfe2e5ed67d5c1
thejandae <tjandae@gmail.com>
2022-02-23 20:20:20 +0800

    update api calls and size adjustments

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,23 @@
 				<div class="left">
 					<span class="number">{{current}}</span>
 					<span>Followers</span>
+					
 				</div>
-				<span class="right number" v-bind:class="{ over: (percentage > 94) }">{{ticker_percentage}}%</span>				
+				<span class="right number" v-bind:class="{ over: (percentage > 85 && percentage <= 92), almost: (percentage >= 92 && percentage < 100), done: percent >= 100 }">{{ticker_percentage}}%</span>				
 			</div>	 		
-			<div class="remaining" :style="`width: ${percentage_remaining}%`" v-bind:class="{ over: (percentage_remaining < 9), fadeIn: (percentage_remaining < 50), fadeOut: (percentage_remaining == 100) }">
-				<span class="number">{{remaining}}</span>
-				<span>to go!</span>
+			<div class="remaining" :style="`width: ${percentage_remaining}%`" v-bind:class="{ over: (percentage_remaining < 32), fadeIn: (percentage_remaining < 50), fadeOuts: (percentage_remaining == 100) }">
+				<span v-show="percentage_remaining > 0" class="number">{{remaining}}</span>
+				<span v-show="percentage_remaining > 0">to go!</span>
+				<span v-show="percentage_remaining <= 0">Thank You!</span>				
 			</div>
-			<div class="goal">				
+			<div class="goal" v-bind:class="{fadeIn: percentage_remaining > 0, fadeOut: percentage_remaining <= 0}">				
 				<span>Goal</span> <span class="number">{{goal}}</span>
 			</div>			
 					
 		</div>		
+		<!-- <div>
+			<iframe style="width: 100%; border: 0; padding-top: 40px" src="https://streamelements.com/overlay/61ca45f8cec1a06d7262f7a5/AcoTvkgwbUf7nlkoAQ97hCS92lCXEybd8AFxb1XkdacTZ1Sz"></iframe>
+		</div> -->
 	</div>
 </template>
 
@@ -35,7 +40,7 @@
 				remaining: 0,
 				percentage_remaining: 100,			
 				ticker_percentage: 0,
-				url: null
+				url: "http://192.168.100.91:1880/follower/goal/data"
 			}
 		},
 		mounted () {
@@ -45,10 +50,10 @@
 			let now = moment("tuesday").format(date_format)
 			console.log(now)
 
-			let parameters_str = window.location.search
-			parameters_str = parameters_str.split('?')[1]
-			this.url = parameters_str.split("url=")[1]
-			console.log(this.url)
+			// let parameters_str = window.location.search
+			// parameters_str = parameters_str.split('?')[1]
+			// this.url = parameters_str.split("url=")[1]
+			// console.log(this.url)
 
 			this.getData()	
 			setInterval(function(){
@@ -59,7 +64,7 @@
 		},
 		methods: {
 			getData() {			
-				this.tickerDown(this.percentage, 0)
+				this.tickerDown(this.percentage, 20)
 				this.percentage_remaining = 80
 				this.percentage = 20
 				// this.current = 0
@@ -70,6 +75,7 @@
 				.get(this.url)
 				.then(response => {
 					let $this = this
+					console.log(response)
 					setTimeout(function(){
 						let data = response.data						
 						$this.current = data.current
@@ -80,7 +86,7 @@
 			},
 			tickerUp(current, max) {			
 				let $this = this				
-
+				// current = current.toFixed(0)
 				let timer = setInterval(function(){
 					if (current <= max) {
 						$this.ticker_percentage = current++
@@ -91,6 +97,7 @@
 			},
 			tickerDown(current, max) {			
 				let $this = this				
+				current = current.toFixed(0)
 
 				let timer = setInterval(function(){
 					if (current >= max) {
@@ -126,10 +133,10 @@
 				}
 			},
 			update_percentage: function () {
-				let percent = parseFloat(((this.current/this.goal)*100))	
-				console.log(typeof percent)		
+				let percent = ((this.current/this.goal)*100)
 				if(!Number.isNaN(percent)){
-					this.percentage = percent.toFixed(2)	
+					// this.percentage = percent.toFixed(2)	
+					this.percentage = percent
 					this.remaining = this.goal - this.current					
 					this.percentage_remaining = (100 - this.percentage) - 4
 					this.tickerUp(0, this.percentage)
@@ -155,24 +162,44 @@
 </script>
 
 <style lang="scss">
+@font-face {
+	font-family: 'amuroregular';
+    src: url('./assets/fonts/amuro-82yn-webfont.woff2') format('woff2'),
+         url('./assets/fonts/amuro-82yn-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.hidden {
+	display: none;
+}
+
+
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	// font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'amuroregular';
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;	
 	color: #fff;		
-	font-size: 20px;
+	font-size: 25px;
 }
 
 
 .bar-wrap {
-	font-family: 'Front Page Neue';	
+	font-family: 'amuroregular';
 	position: relative;
 	background: #232323;
+	width: 750px;
 	height: 54px;
 	display: flex;
     justify-content: flex-end;
 	border-radius: 10px;
     overflow: hidden;    
+	box-shadow: 0px 1px 14px 2px rgba(0,0,0,0.46);
+	-webkit-box-shadow: 0px 1px 14px 2px rgba(0,0,0,0.46);
+	-moz-box-shadow: 0px 1px 14px 2px rgba(0,0,0,0.46);
+	text-transform: uppercase;
+	text-shadow: 0px 0px 4px rgba(0,0,0,0.62);
 
 	.goal {
 		display: flex;
@@ -181,8 +208,17 @@
 		z-index: 2;
 		// background: #232323;
 		padding-right: 10px;
+		transition: opacity 2s ease-in-out;
 		span {
 			margin-right: 5px;
+		}
+
+		&.fadeIn {
+			opacity: 1;
+		}
+
+		&.fadeOut {
+			opacity: 0;
 		}
 	}
 
@@ -198,6 +234,7 @@
 		overflow: visible;
 		z-index: 1;
 		opacity: 0;
+		background: r;
 
 		box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
 		transform: scale(1);
@@ -222,6 +259,8 @@
 	}
 }
 
+
+
 .bar {
 	position: absolute;
 	left: 0;
@@ -229,7 +268,7 @@
 	display: flex;
 	justify-content: space-between;
 	background-color: #7f00ce;
-	padding: 10px;
+	padding: 9px 10px;
 	transition: width 2s;
 	// background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
 	background: linear-gradient(-45deg, #450059, #662e77, #ff9976, #ff784f);
@@ -249,14 +288,22 @@
 
 	.right {
 		&.over {
-			padding-right: 6%;
+			padding-right: 3.5%;
+		}
+
+		&.almost {
+			padding-right: 9.5%;
+		}
+
+		&.done {
+			padding-right: 15px;
 		}
 	}
 }
 
 
 .number {
-	font-size: 1.5em;
+	font-size: 1.1em;
 }
 
 @keyframes gradient {
